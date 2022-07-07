245fd7ca8907cdc502b0671613dca194da8e33b4
Amir Iskandar <amiriskandar@graduate.utm.my>
2022-07-08 03:32:26 +0800

    donation history complete

---
+++ Donor/donationhistory.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Donate4All-Donation History</title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis:400,500|Poppins:400,700&amp;display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body class="sub_page">
+    <div class="hero_area">
+        <!-- header section strats -->
+        <?php include('include/mainheader.php'); ?>
+        <!-- end header section -->
+    </div>
+
+    <!-- start profile -->
+    <br>
+    <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                <div class="card-title">
+                                    <h4>Donation History Table </h4>
+                                </div>
+                                <div class="bootstrap-data-table-panel">
+                                    <div class="table-responsive">
+                                        <table id="row-select" class="display table table-borderd table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>Donation ID</th>
+                                                    <th>Amount</th>
+                                                    <th>Status</th>
+                                                    <th>Donation Date</th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                <?php include 'include/retrievedonation.php'; ?>
+
+                                                <?php if ($result->num_rows > 0) : ?>
+
+                                                    <?php while ($array = mysqli_fetch_row($result)) : ?>
+
+                                                        <tr>
+                                                            <th scope="row"><?php echo $array[0]; ?></th>
+                                                            <td>RM <?php echo $array[1]; ?></td>
+                                                            <td><?php 
+                                                            if($array[2]==1)
+                                                            echo "Has been received by recipient";
+                                                            else
+                                                            echo "Not been received by recipient"; ?></td>
+                                                            <td><?php echo $array[3]; ?></td>
+                                                        </tr>
+                                                    <?php endwhile; ?>
+                                                <?php else : ?>
+                                                    <tr>
+                                                        <td colspan="3" rowspan="1" headers="">No Data Found</td>
+                                                    </tr>
+                                                <?php endif; ?>
+                                                <?php mysqli_free_result($result); ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
+        <br>
+    <!-- end form -->
+    <br><br><br><br>
+    <div class="footer_bg">
+        <!-- footer section -->
+        <section class="container-fluid footer_section">
+            <p>
+                © 2022 All Rights Reserved By
+                <a href="index.html">Donate4All</a>
+            </p>
+        </section>
+        <!-- footer section -->
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
