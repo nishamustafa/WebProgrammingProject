0db39196d1c7e35c3705b5bdaadf7f1c3809329e
ShahRenin <shahril01@graduate.utm.my>
2022-07-08 01:38:31 +0800

    fuck

--- Donor/home.php
+++ Donor/home.php
@@ -155,46 +155,7 @@ check_login();
       <!-- end slider section -->
     </div>
 
-<<<<<<< HEAD:Donor/home.php
     <?php include('include/mainwelcome.php');?>
-=======
-    <!-- welcome section -->
-    <section class="welcome_section layout_padding">
-      <div class="container">
-        <div class="custom_heading-container">
-          <h2>Welcome To Donate 4 All</h2>
-        </div>
-        <div class="layout_padding2">
-          <div class="img-box">
-            <img src="images/welcome.jpg" alt="" />
-          </div>
-          <div class="detail-box">
-            <p>
-              A survey conducted by United Nations agencies UNICEF and
-              UNFPA in the near end of 2020, when Malaysia underwent the conditional movement control order (CMCO), it is found that
-              the socio-economic state of an approximate half of the Malaysian
-              households were gradually deteriorating due to them nearing their
-              breaking points. These surveys have found that unemployment in
-              the country has doubled up to 15% when compared to three months
-              prior the occurence, where one-third of a household remains
-              unemployed. <br/><br/>
-
-              Due to the current state of unemployment and loss of jobs of the
-              public since COVID-19 occured, Donate4All takes the advantage
-              to help the families and those who are in need in a rough time.
-              Donate4All serves as a platform for users to donate money and
-              items as per needed by families who could not afford, as well as
-              a platform for those who requires aid to afford basic necessities.
-            </p>
-            <div>
-              <a href=""> Read More </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- end welcome section -->
->>>>>>> 9d4423f5ca1bc36b025215b3d6499a8fb92bda2c:Donor/home.html
 
     <div class="footer_bg">
       <!-- footer section -->
