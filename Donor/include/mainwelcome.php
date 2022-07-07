93213d758dd29e243a720ced801026206b4221b3
ShahRenin <shahril01@graduate.utm.my>
2022-07-08 01:34:18 +0800

    f

--- Donor/include/mainwelcome.php
+++ Donor/include/mainwelcome.php
@@ -2,19 +2,29 @@
 <section class="welcome_section layout_padding">
       <div class="container">
         <div class="custom_heading-container">
-          <h2>Welcome To Our Website</h2>
+          <h2>Welcome To Donate 4 All</h2>
         </div>
         <div class="layout_padding2">
           <div class="img-box">
-            <img src="images/welcome.png" alt="" />
+            <img src="images/welcome.jpg" alt="" />
           </div>
           <div class="detail-box">
             <p>
-              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-              voluptate velit esse cillum dolore eu
+              A survey conducted by United Nations agencies UNICEF and
+              UNFPA in the near end of 2020, when Malaysia underwent the conditional movement control order (CMCO), it is found that
+              the socio-economic state of an approximate half of the Malaysian
+              households were gradually deteriorating due to them nearing their
+              breaking points. These surveys have found that unemployment in
+              the country has doubled up to 15% when compared to three months
+              prior the occurence, where one-third of a household remains
+              unemployed. <br/><br/>
+
+              Due to the current state of unemployment and loss of jobs of the
+              public since COVID-19 occured, Donate4All takes the advantage
+              to help the families and those who are in need in a rough time.
+              Donate4All serves as a platform for users to donate money and
+              items as per needed by families who could not afford, as well as
+              a platform for those who requires aid to afford basic necessities.
             </p>
             <div>
               <a href=""> Read More </a>
