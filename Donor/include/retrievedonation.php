5afa3e3430121197d51b0413fb05a6a3a6bf30f4
Amir Iskandar <amiriskandar@graduate.utm.my>
2022-07-08 14:59:36 +0800

    tidying up

--- Donor/include/retrievedonation.php
+++ Donor/include/retrievedonation.php
@@ -1,5 +1,5 @@
 <?php
 include 'include/config.php';
-$query="select donationID,Amount,Status,DonationDate from donation";
+$query="select donationID,Amount,Status,DonationDate from donation where donor_fk='". $_SESSION['id'] ."'";
 $result=mysqli_query($con,$query);
 ?>
